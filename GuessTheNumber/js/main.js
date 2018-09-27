f33c64984bed36412af2f876b8edbdd1f7019b65
theweirdgenie <dheeraj_chettri@yahoo.com>
2018-09-27 20:06:12 +0530

    changes

--- GuessTheNumber/js/main.js
+++ GuessTheNumber/js/main.js
@@ -8,38 +8,34 @@ function PlayGame()
 	var count=0;
 	var inp = window.prompt("Guess the number between 1 & 20");
 	
-	while(isNaN(inp))
-	{
-		inp = window.prompt("Please enter a number");
-	}
 
 	while(inp != num){
+		
+
+	if(isNaN(inp))
+		inp = window.prompt("Please enter a number");
 	if(inp == null)
-	{
 		return;
-	}
-
-
-
-	while (inp>num)
+	if (inp>num)
 	{
 		alert("Try lower number");
 		inp = window.prompt("Try again. Guess the number.");
 		count++;
 	}
-
-	while (inp<num)
+	if (inp<num)
 
 	{
 		alert("Try higher number");
 		inp = window.prompt("Try again. Guess the number.");
 		count++;
 	}
-		
+	
 	}
-
 		alert("Congrats! You've got it!");
 		count++;
+		
+			
+
 
 		if(count<=3)
 		{
@@ -59,7 +55,16 @@ function PlayGame()
 		}
 
 
-}
+
+
+
+		
+	}
+
+
+
+
+
 
 $("#gameButton").click(function(){
 
@@ -68,3 +73,44 @@ $("#gameButton").click(function(){
 	
 
 });
+
+
+
+function PalindromeChecker(str)
+{
+	var arr =[];
+	var revArr =[];
+	str = str.toLowerCase();
+	for(var i=0; i<str.length; i++)
+	{
+		arr.push(str.charAt(i));
+	}
+
+	revArr = arr.reverse();
+
+
+	var str1 = revArr.join("");
+
+
+	if(str1 == str)
+
+	{
+		return true;
+	}
+	else
+	{
+	
+		return false;
+	}
+
+
+
+
+}
+
+console.log(PalindromeChecker("winf"));
+console.log(PalindromeChecker("racecar"));
+console.log(PalindromeChecker("cat"));
+console.log(PalindromeChecker("dog"));
+console.log(PalindromeChecker("anna"));
+
